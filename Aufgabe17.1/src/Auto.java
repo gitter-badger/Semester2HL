8dd40029f0387457e3f10ac397f5e93b6a75ad04
Eiamnacken <Sven2q@hotmail.de>
2015-02-19 18:56:58 +0100

    Aufgabe 17.1 Fertig
    Die aufgabe 17.1 ist komplett fertig und einsatzbereit

--- Aufgabe17.1/src/Auto.java
+++ Aufgabe17.1/src/Auto.java
@@ -4,7 +4,7 @@
  * @author Sven Marquardt
  *
  */
-public class Auto extends Bestand {
+public class Auto  {
 	private String hersteller;
 	private long laufleistung;
 	private double preis;
@@ -12,6 +12,7 @@ public class Auto extends Bestand {
 	private boolean unfallwagen;
 	private String kraftstoff;
 	private double psleistung;
+	private Bestand bestand;
 
 	/**
 	 * Konstruktur für Klasse Auto
@@ -34,19 +35,19 @@ public class Auto extends Bestand {
 	public Auto(String hersteller, long laufleistung, double preis,
 			String farbe, boolean unfallwagen, String kraftstoff,
 			double psleistung) {
-		this.hersteller = hersteller;
-		this.laufleistung = laufleistung;
-		this.preis = preis;
-		this.farbe = farbe;
-		this.unfallwagen = unfallwagen;
-		this.kraftstoff = kraftstoff;
-		this.psleistung = psleistung;
+		this.setHersteller(hersteller);
+		this.setLaufleistung(laufleistung);
+		this.setPreis(preis);
+		this.setFarbe(farbe);
+		this.setUnfallwagen(unfallwagen);
+		this.setKraftstoff(kraftstoff);
+		this.setPsleistung(psleistung);
 
 	}
 
 	/**
 	 * Fügt ein Auto dem Bestand hinzu durch prüfen mit
-	 * {@link #istVorhanden(Auto)} und fügt es dann mit {@link #aufnehmen(Auto)}
+	 * {@link #istVorhanden(Auto)} und fügt es dann mit {@link #}
 	 * hinzu
 	 * 
 	 * @param bestand
@@ -55,19 +56,23 @@ public class Auto extends Bestand {
 	 * @see #istVorhanden(Auto)
 	 */
 	public boolean zumBestandHinzu(Bestand bestand) {
-		return true;
+		if(bestand.aufnehmen(this)) return true;
+		return false;
 	}
 
 	/**
-	 * entfernt ein Auto aus dem Bestand prueft vorher mit
-	 * {@link #istVorhanden(Auto)} und entimmmt es mit {@link #entnehmen(Auto)}
+	 * entfernt ein Auto aus dem Bestand
+	 * prueft vorher mit
+	 * {@link #istVorhanden(Auto)} und entimmmt es mit {@link #entnehmen(Auto)} 
+	 * danach ist das Auto aus dem Bestand entfernt
 	 * 
 	 * @return true oder false wenn Auto entfernt erfolgreich war oder nicht 
 	 * @see #istVorhanden(Auto)
 	 * @see #entnehmen(Auto)
 	 */
 	public boolean ausDemBestand() {
-		return true;
+		if(bestand.entnehmen(this)) return true;
+		return false;
 	}
 
 	/**
@@ -76,6 +81,74 @@ public class Auto extends Bestand {
 	 * @return Ein Auto als String wie in Aufgabe 15.1 beschrieben
 	 */
 	public String toString() {
-		return "";
+		return "-------\nHersteller: " + this.hersteller + "\n" + "Preis: "
+				+ this.preis + "€\n" + "Motor: " + this.psleistung + "PS ("
+				+ this.kraftstoff + ")\n" + "KM-Stand: " + this.laufleistung
+				+ " km\n" + "Farbe: " + this.farbe + "\n"
+				+ (!this.unfallwagen?"unfallfrei\n--------\n":"--------\n");
+	}
+
+	public String getHersteller() {
+		return hersteller;
+	}
+
+	public void setHersteller(String hersteller) {
+		this.hersteller = hersteller;
+	}
+
+	public long getLaufleistung() {
+		return laufleistung;
+	}
+
+	public void setLaufleistung(long laufleistung) {
+		this.laufleistung = laufleistung;
+	}
+
+	public String getFarbe() {
+		return farbe;
+	}
+
+	public void setFarbe(String farbe) {
+		this.farbe = farbe;
+	}
+
+	public boolean isUnfallwagen() {
+		return unfallwagen;
+	}
+
+	public void setUnfallwagen(boolean unfallwagen) {
+		this.unfallwagen = unfallwagen;
+	}
+
+	public String getKraftstoff() {
+		return kraftstoff;
+	}
+
+	public void setKraftstoff(String kraftstoff) {
+		this.kraftstoff = kraftstoff;
+	}
+
+	public double getPsleistung() {
+		return psleistung;
+	}
+
+	public void setPsleistung(double psleistung) {
+		this.psleistung = psleistung;
+	}
+
+	public Bestand getBestand() {
+		return bestand;
+	}
+
+	public void setBestand(Bestand bestand) {
+		this.bestand = bestand;
+	}
+
+	public double getPreis() {
+		return preis;
+	}
+
+	public void setPreis(double preis) {
+		this.preis = preis;
 	}
 }
