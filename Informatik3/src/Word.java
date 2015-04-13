9b4af0fb0fc65a3dce6d03b5b524b30fd2f1ff73
eiamnacken <sven2q@hotmail.de>
2015-04-13 11:47:14 +0200

    Aufgabe initiiert scanner.hasnext() blockkiert das programm ansonsten fertiggestellt das ganze
    IDEE!!
    
    Könnte ein menü einbauen um die datei asuzuwählen vielleicht in gui mal
    schauen

---
+++ Informatik3/src/Word.java
@@ -0,0 +1,39 @@
+
+public class Word implements Comparable<Word> {
+	
+	private String content;
+	private int n=0;
+	
+	public Word(String s) {
+		this.setContent(s);
+		this.n=1;
+	}
+	
+	public int count(){ return this.n;}
+	
+	public void inc(){ this.n++;}
+	
+
+	@Override
+	public int compareTo(Word w) {
+		if(this.content.equalsIgnoreCase(w.toString())){
+			return 0;
+		}else if(this.content.length()>w.toString().length()){
+			return 1;
+		}
+		return -1;
+	}
+
+	public String getContent() {
+		return this.content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public String toString() {
+		return this.getContent()+": "+this.count();
+	}
+
+}
